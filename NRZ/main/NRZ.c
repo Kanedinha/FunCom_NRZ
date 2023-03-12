a0c8cc4584e3a60ea211e29e4f18ee4ae484bd9d
Kanedinha <emerson.k337@gmail.com>
2023-03-12 20:33:51 -0300

    overflow task and return task problens

--- NRZ/main/NRZ.c
+++ NRZ/main/NRZ.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/param.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+#include <rom/ets_sys.h>
+#include <esp_types.h>
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/timers.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "esp_system.h"
 #include "esp_log.h"
+#include <esp_task_wdt.h>
+#include <esp_rom_gpio.h>
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_heap_caps.h"
+
+#include "nvs_flash.h"
 
 #define CLK 14
 #define TX 13
@@ -12,12 +33,13 @@ static const char *TAG = "NRZ";
 
 bool clockState = 0;
 
-void clock_generate()
+void clock_generate(void *arg)
 {
     vTaskDelay(200 / portTICK_PERIOD_MS);
     clockState = !clockState;
     gpio_set_level(CLK, clockState);
     ESP_LOGI(TAG, "clock state: %d", clockState);
+    vTaskDelete(NULL);
 }
 
 void peripherical_init()
@@ -27,10 +49,21 @@ void peripherical_init()
 
 void app_main()
 {
+    esp_err_t ret;
+
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
     peripherical_init();
-    xTaskCreate(&clock_generate, "clock_generate", 1024, NULL, 5, NULL);
+
     while (1)
     {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        xTaskCreate(&clock_generate, "clock_generate", 1024, NULL, 5, NULL);
+
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
